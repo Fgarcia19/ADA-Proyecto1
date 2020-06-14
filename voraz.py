0cfa115cdc149bc855d3b7c4c5caf05ab47d5f9e
Fgarcia19 <40769836+Fgarcia19@users.noreply.github.com>
2020-06-13 19:40:53 -0500

    Update voraz.py

--- voraz.py
+++ voraz.py
@@ -1,5 +1,3 @@
-matchings=[]
-
 def bloques(A):
     A_bloques = []
     inicio = -1
@@ -13,70 +11,127 @@ def bloques(A):
             A_bloques.append((inicio, fin))
             inicio = -1
             def_inicio=False
+
+    if(def_inicio):
+        A_bloques.append((inicio,len(A)-1))
     return A_bloques
 
-def peso(A,B):
-    if len(A)==1:
-        return A/sum(B)
-    else:
-        return sum(A)/B
+def peso(A,B,m):
+    v_A=0
+    v_B=0
+    t_A=-1
+    t_B=-1
+    for i in m:
+        if(i[0]!=t_A):
+            v_A+=valor(A[i[0]])
+            t_A=i[0]
+        if(i[1]!=t_B):
+            v_B+=valor(B[i[1]])
+            t_B=i[1]
+    return (v_A/v_B)
+
 
 def valor(a):
     return a[1]-a[0]+1
 
 
 def iterar(A,B):
+    peso_m=0
+    matchings=[]
+    match=[]
     j=0
     i=0
     sum1=valor(A[0])
     sum2=valor(B[0])
-    match=[]
     while(i<len(A)):
-        match.append((i,j))
+        match.append((i, j))
+        print("A ",i,end=' ')
+        print("B ",j)
         if j==len(B)-1:
             i+=1
+        elif i==len(A)-1:
+            j+=1
+        elif sum1 == sum2:
+            print("A ", i, end=' ')
+            print("B ", j)
+            i += 1
+            j += 1
+            match.append((i, j))
+            peso_m += peso(A, B, match)
+            matchings.append(match)
+            match=[]
         elif sum1 > sum2:
             j+=1
-            sum2 += valor(B[j])
-            if sum1<=sum2:
-                match.append((i, j))
-                matchings.append(match)
-                match=[]
+            if j<len(B)-1:
+                sum2 += valor(B[j])
+                if sum1<=sum2 :
+                    print("A ", i, end=' ')
+                    print("B ", j)
+                    match.append((i, j))
+                    peso_m += peso(A, B, match)
+                    matchings.append(match)
+                    match=[]
+                    i+=1
+                    if i==len(A):
+                        break;
+                    j+=1
+                    if j==len(B):
+                        break;
+                    sum1 = valor(A[i])
+                    sum2 = valor(B[j])
+            else:
                 i+=1
-                j+=1
-                sum1 = valor(A[i])
-                sum2 = valor(B[j])
-        else:
+        elif sum1 < sum2:
             i+=1
-            sum1 += valor(A[i])
-            if sum1 >= sum2:
-                match.append((i, j))
-                i += 1
-                j += 1
-                matchings.append(match)
-                match=[]
-                sum1 = valor(A[i])
-                sum2 = valor(B[j])
+            if i<len(A)-1:
+                sum1 += valor(A[i])
+                if sum1 >= sum2:
+                    print("A ", i, end=' ')
+                    print("B ", j)
+                    match.append((i, j))
+                    peso_m += peso(A, B, match)
+                    matchings.append(match)
+                    match = []
+                    i += 1
+                    if i==len(A):
+                        break;
+                    j += 1
+                    if j==len(B):
+                        break;
+                    sum1 = valor(A[i])
+                    sum2 = valor(B[j])
+            else:
+                j+=1
+    peso_m+=peso(A,B,match)
     matchings.append(match)
     print(matchings)
-    print("Los conjuntos de matching son")
-    for i in range(0,len(matchings)):
-        print("Match ",i,end=' ')
-        print(matchings[i])
+    print(peso_m)
 
 
 
 def voraz():
-    A=[0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0]
-    B=[0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0]
 
+    A=[0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0]
+    # A=[0,1,0,1,0,1,0,1,0,1]
+    # B=[1,1,1,1,1,0,1,0]
+    # A=[0,1,0,1,1,0,0,1,1,1,1,1,1,0,1]
+    # B=[0,1,1,1,0,1,0,1,0]
+    B=[0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0]
+    # A=[0, 1 , 0 , 0 , 1 , 1, 0 , 1 , 0 , 1 , 1 , 1 , 0 , 1 , 1 , 0 , 1]
+    # B=[0, 0,  1 , 1 , 0 , 1 , 1, 0 , 1 , 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0]
     bloquesA=bloques(A)
     bloquesB=bloques(B)
-
+    print(bloquesA)
+    print(bloquesB)
     if len(bloquesA)> len(bloques(B)):
         iterar(bloquesA,bloquesB)
     else:
         iterar(bloquesB,bloquesA)
+
+
+
+
+   # print(sum([1,2,3]))
 
 
 if __name__ == "__main__":
